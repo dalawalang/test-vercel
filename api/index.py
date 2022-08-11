df21986416318b9dd3db6b7d62b10e4ebfd49ec1
Noizrom <noizrom@gmail.com>
2022-08-11 22:51:44 +0800

    c

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
-from http.server import BaseHTTPRequestHandler
+# from http.server import BaseHTTPRequestHandler
 
 
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write("Im Returning from you".encode())
-        return
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type','text/plain')
+#         self.end_headers()
+#         self.wfile.write("Im Returning from you".encode())
+#         return
+
+from fastapi import FastAPI , Request
+import json
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def index(request: Request):
+    return json({'hello': request})
+
+@app.get('/{test}')
+async def index(request: Request , test: str):
+    return json({'hello': request , 
+                 'test': test})
