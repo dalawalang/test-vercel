f6a25111d47b812a1d8f60620de14f0500ddd38f
Noizrom <noizrom@gmail.com>
2022-08-11 23:15:56 +0800

    f

--- api/[great].py
+++ api/[great].py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get('/')
 async def index(request: Request):
-    print('request ', request)
-    return {'just visiting': str(request.url)}
+    print('request ', dir(request))
+    return {'this is the message boi': str(request['path'])}
