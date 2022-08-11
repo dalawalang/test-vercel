74593c7fe8e1e98f887b098fb02091d2df659be6
Noizrom <noizrom@gmail.com>
2022-08-11 22:58:44 +0800

    c

--- api/index.py
+++ api/index.py
@@ -11,17 +11,14 @@
 #         return
 
 from fastapi import FastAPI , Request
-import json
-
 
 app = FastAPI()
 
 
 @app.get('/')
 async def index(request: Request):
-    return json({'hello': request})
+    return {'hello': request}
 
 @app.get('/{test}')
 async def index(request: Request , test: str):
-    return json({'hello': request , 
-                 'test': test})
+    return {'hello': request , 'test': test}
