21d2d19551a3cdd2b004e772cf6b379ddf73a321
Noizrom <noizrom@gmail.com>
2022-08-12 17:35:11 +0800

    k

--- api/index.py
+++ api/index.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI , Request
 
 app = FastAPI()
 
+@app.get('/api/{function}')
+async def some_data(function: str):
+    return {'function': function ,'status': 200}
 
-@app.get('/')
-async def index(request: Request):
-    return {'nospace': 'nothing to see here'}
 
-
-@app.get('/{data}')
-async def some_data(data: str):
-    return {'data': data ,'status': 200}
+@app.get('/api/test/{file}')
+async def some_data(file: str):
+    return {'file': file ,'status': 401}
